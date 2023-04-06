8966ed029230e410389039d731312910ed9f155e
Aguilera2509 <ja846699@gmail.com>
2023-04-06 16:59:02 -0400

    Functions of a calculator in TS

---
+++ src/calculator.ts
@@ -0,0 +1,74 @@
+export class calculate{
+    result:number = 0;
+    currentOperand:string = "";
+    case:string = "";
+    previousOperand:string = "";
+    newValues:boolean = true;
+
+    Display(value:string):string{
+        this.newValues = true;
+        this.currentOperand = this.currentOperand + value;
+        return value;
+    };
+
+    Operation(operator:string):string{
+        this.newValues = true;
+        if(this.currentOperand === "") return "";
+        if(this.previousOperand !== ""){
+            this.Computing();
+        };
+        this.case = operator;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
+        
+        return operator;
+    };
+    
+    Computing():number{
+        if(this.case === ""){
+            this.Clear();
+            return 0; 
+        };
+        if(isNaN(parseFloat(this.previousOperand)) || isNaN(parseFloat(this.currentOperand))) return 0;
+
+        switch (this.case) {
+            case "+":
+                this.result = parseFloat(this.previousOperand) + parseFloat(this.currentOperand);
+                break;
+            case "-":
+                this.result = parseFloat(this.previousOperand) - parseFloat(this.currentOperand);
+                break;
+            case "*":
+                this.result = parseFloat(this.previousOperand) * parseFloat(this.currentOperand);
+                break;
+            case "/":
+                this.result = parseFloat(this.previousOperand) / parseFloat(this.currentOperand);
+                break;  
+            default:
+                break;
+        };
+
+        this.currentOperand = String(this.result);
+        this.case = "";
+        this.previousOperand = "";
+        this.newValues = false;
+
+        return this.result;
+    };
+
+    Back(display:string):string{
+        let modified:string = "";
+        for(let index = 0; index < display.length - 1; index++) {
+            modified += display[index];
+        };
+        this.currentOperand = modified;
+        return modified || "0";
+    };
+
+    Clear(){
+        this.case = "";
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.result = 0;
+    };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,45 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { calculate } from "./calculator.js";
 
-import confetti from 'canvas-confetti';
+const $keys = document.querySelectorAll<HTMLButtonElement>("[data-number]");
+const $operation = document.querySelectorAll<HTMLButtonElement>("[data-operation]");
+const $display = document.getElementById("display");
+const $displayOperation = document.getElementById("displayOperation");
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+let calculator = new calculate();
+
+$keys.forEach(el => {
+    el.addEventListener("click", e => {
+        if(!calculator.newValues){
+            $display!.textContent = "0";
+            calculator.Clear();
+            $display!.textContent = calculator.Display(String(el.textContent));
+            calculator.newValues = true;
+            return;
+        };
+
+        if($display?.textContent === "0"){
+            return $display.textContent = calculator.Display(String(el.textContent));
+        }else{
+            $display!.textContent += calculator.Display(String(el.textContent));
+        };
+    });
+});
+
+$operation.forEach(el => {
+    el.addEventListener("click", e => {
+        if(el.textContent === "=" && $display!.textContent!.length >= 3){
+            $displayOperation!.textContent = `${calculator.previousOperand}  ${calculator.case}  ${calculator.currentOperand}`; 
+            let result = calculator.Computing();
+            return $display!.textContent = String(result);
+        }else if(el.textContent === "C"){
+            $display!.textContent = "0";
+            return calculator.Clear();
+        }else if(el.textContent === "E"){
+           return $display!.textContent = calculator.Back(String($display!.textContent));
+        }else if(el.textContent === "."){
+            return $display!.textContent += calculator.Display(String(el.textContent));
+        };
+        
+        $display!.textContent += calculator.Operation(String(el.textContent));
+    });
+});
